057341b3b9f327d84782383f4709e8448e6d2c06
Darren Tebo <darrentebo83@outlook.com>
2020-05-18 23:19:57 -0400

    broke down buttons into separate component

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import TopRow from './TopRow';
 import BottomRow from "./BottomRow";
+import Buttons from './Buttons';
 
 const Scoreboard = () => {
     //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -13,22 +14,10 @@ const Scoreboard = () => {
                 <TopRow home={homeScore} away={awayScore} />
                 <BottomRow />
             </section>
-            <section className="buttons">
-                <div className="homeButtons">
-
-                {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-                <button className="homeButtons__touchdown"
-                        onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
-                <button className="homeButtons__fieldGoal"
-                        onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
-                </div>
-                <div className="awayButtons">
-                <button className="awayButtons__touchdown"
-                        onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
-                <button className="awayButtons__fieldGoal"
-                        onClick={() => setAwayScore(awayScore + 3)}>Away Field Goal</button>
-                </div>
-            </section>
+            <Buttons incrementHomeScore={setHomeScore}
+                    incrementAwayScore={setAwayScore}
+                    home={homeScore}
+                    away={awayScore} />
         </div>
     );
 };
